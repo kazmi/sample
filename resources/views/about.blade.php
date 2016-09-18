80f9f50ce42c72e6762decf76af108c41bdfa2d3
Sulaiman Azhar <sulaiman.azhar@cloudiris.ae>
2016-09-18 17:50:57 +0400

    add style

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -1,3 +1,14 @@
-<h1>About</h1>
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+	<link rel="stylesheet" href="/css/app.css">
+</head>
+<body>
 
-<p>Hello, World</p>
+	<h1>About</h1>
+
+	<p>Hello, World</p>
+
+</body>
+</html>
